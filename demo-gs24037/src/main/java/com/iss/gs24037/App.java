641c134ccba393f8afd9e0cffca0a2a6116adc10
Your Name <you@example.com>
2024-05-02 20:34:11 -0600

    My first commit

---
+++ demo-gs24037/src/main/java/com/iss/gs24037/App.java
@@ -0,0 +1,17 @@
+package com.iss.gs24037;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	public String Hello() {
+		return "Hola Mundo";
+}
+    public static void main( String[] args )
+    {
+    	App hw = new App();
+    	System.out.println(hw.Hello());
+    	}
+}
---
+++ demo-gs24037/src/test/java/com/iss/gs24037/AppTest.java
@@ -0,0 +1,22 @@
+package com.iss.gs24037;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void testHello()
+    {
+        assertTrue( true );
+    }
+}
